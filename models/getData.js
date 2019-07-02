65c7d7ee3e0f92aec5ef942b1594531e0ac554ab
johnmuthua <muthuahjohn@gmail.com>
2019-07-02 17:50:28 +0300

    created models to separate the files

---
+++ models/getData.js
@@ -0,0 +1,42 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+var mysql = require('mysql')
+
+document.getElementById('btn').addEventListener("click", () =>{
+    var mysql = require("mysql");
+
+    //Database credentials
+    var connection = mysql.createConnection({
+      host : 'localhost',
+      user : 'root',
+      password: null,
+      database : 'add'
+    });
+
+    //connection
+    connection.connect((err) => {
+      if(err){
+        return console.log(err.stack);
+      }
+      console.log('Connection successfully established');
+    });
+    
+    $queryString = 'SELECT id,title FROM `documents` WHERE 1 ;';
+    connection.query($queryString, (err,rows,fields) =>{
+      if(err){
+        return console.log('An error occured',err);
+      }
+
+      for (var index = 0; index < 20; index++){
+        console.log(rows[index]['title']);
+        document.getElementById('demo').innerHTML += rows[index]['title'];
+        document.getElementById('demo').innerHTML += '<br/>'
+      }
+
+      console.log(rows.length);
+    });
+    //close connection
+    connection.end(() =>{
+      console.log("Connection terminaned")
+    });
+  }, false)
